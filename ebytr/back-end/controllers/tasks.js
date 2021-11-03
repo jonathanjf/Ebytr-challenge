739b0311e48784e501d2c1113e35f86c4bd49ab3
jonathan <jonathanfernandesjf98@gmail.com>
2021-11-03 18:25:24 -0300

    instalacao da dependencia mongodb, e inclusao do async nos controllers

--- ebytr/back-end/controllers/tasks.js
+++ ebytr/back-end/controllers/tasks.js
@@ -1,23 +1,23 @@
 const tasksService = require('../services/tasks');
 
-const getAll = (_req, res) => {
+const getAll = async (_req, res) => {
   const tasks = await tasksService.getAll();
   res.status(200).json(tasks);
 };
 
-const create = (req, res) => {
+const create = async (req, res) => {
   const { task } = req.body;
   const taksCreated = await tasksService.create({ task });
   res.status(201).json(taksCreated);
 }
 
-const update = (req, res) => {
+const update = async (req, res) => {
   const { id } = req.params;
   const taskUpdated = await tasksService.update(id);
   res.status(201).json(taskUpdated);
 }
 
-const remove = (req, res) => {
+const remove = async (req, res) => {
   const { id } = req.params;
   await tasksService.remove(id);
   res.status(200).json( { message: 'tarefa excluida com sucesso' } );
