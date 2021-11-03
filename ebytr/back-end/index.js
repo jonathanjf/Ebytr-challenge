907a810867e497550a1c8eedc6270aa4e94772bb
jonathan <jonathanfernandesjf98@gmail.com>
2021-11-03 17:16:13 -0300

    instalacao de dependencias

--- ebytr/back-end/index.js
+++ ebytr/back-end/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 
-const app = express(); // 1
-
-app.get('/hello', handleHelloWorldRequest); // 2
+const app = express();
 
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
