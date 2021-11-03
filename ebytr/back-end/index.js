0600b686a8c6fd6838b49a5528185e0e28d49f24
jonathan <jonathanfernandesjf98@gmail.com>
2021-11-03 18:26:24 -0300

    instalacao da dependencia mongodb, e inclusao do async nos controllers alem da criacao da rota get /tasks no index

--- ebytr/back-end/index.js
+++ ebytr/back-end/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const tasksController = require('./controllers/tasks');
 
 const app = express();
+
+app.use(bodyParser());
+
+app.get('/tasks', tasksController.getAll);
 
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
