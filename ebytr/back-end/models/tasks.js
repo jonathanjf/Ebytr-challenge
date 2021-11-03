fb6150cf04df982b6a244a48fd5f0e7abcff057b
jonathan <jonathanfernandesjf98@gmail.com>
2021-11-03 17:36:05 -0300

    criacao das rotas para criacao e atualizacao de tarefas

--- ebytr/back-end/models/tasks.js
+++ ebytr/back-end/models/tasks.js
@@ -3,6 +3,13 @@ const connection = require('./connection');
 const getAll = async () => connection()
   .then((db) => db.collection('tasks').findAll({}).toArray());
 
+const create = async (task) => connection()
+  .then((db) => db.collection('tasks').insertOne(task));
+
+const update = async (id) => connection()
+  .then((db) => db.collection('tasks').updateOne(id));
+
 module.exports = {
   getAll,
+  create,
 }
